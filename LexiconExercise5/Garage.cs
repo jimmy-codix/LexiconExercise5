8bf55bcf61192d6ce8b00ad98e9b5d5fab37af3e
Jimmy <212524470+jimmy-codix@users.noreply.github.com>
2025-06-14 12:31:01 +0200

    Added some props and methods to Garage and made an Interface.

--- LexiconExercise5/Garage.cs
+++ LexiconExercise5/Garage.cs
@@ -1,9 +1,45 @@
-﻿namespace LexiconExercise5
+﻿using LexiconExercise5.Interfaces;
+using System.Collections;
+
+namespace LexiconExercise5
 {
-    internal class Garage
+    internal class Garage<T> : IGarage<T>, IEnumerable<T> where T : class
     {
-        public Garage()
+        private T[] _items;
+        public int Count { get; private set; }
+        public T this[int index]
         {
+            get
+            {
+                if (index < 0 || index >= Count)
+                    throw new IndexOutOfRangeException();
+                return _items[index];
+            }
+            set
+            {
+                if (index < 0 || index >= Count)
+                    throw new IndexOutOfRangeException();
+                _items[index] = value;
+            }
+        }
+
+        public Garage(int size)
+        {
+            _items = new T[size];
+            Count = size;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return _items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }
---
+++ LexiconExercise5/Interfaces/IGarage.cs
@@ -0,0 +1,11 @@
+﻿namespace LexiconExercise5.Interfaces
+{
+    internal interface IGarage<T> where T : class
+    {
+        T this[int index] { get; set; }
+
+        int Count { get; }
+
+        IEnumerator<T> GetEnumerator();
+    }
+}
