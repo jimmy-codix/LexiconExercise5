30e06cc456472e25efadf71beba84d24187bfcb0
Jimmy <212524470+jimmy-codix@users.noreply.github.com>
2025-06-16 07:49:37 +0200

    Added methods to Manager.

--- LexiconExercise5/Manager.cs
+++ LexiconExercise5/Manager.cs
@@ -1,9 +1,71 @@
-﻿namespace LexiconExercise5
+﻿using LexiconExercise5.Vehicles;
+
+namespace LexiconExercise5
 {
+    //Start point for Application.
     internal class Manager
     {
+        private Handler _handler;
+        //private Garage<> _garage;
+        private Garage<Vehicle> _garage;
+
+        //Menus
+        private List<Action> _garageMenu;
+
         public Manager()
         {
+            _handler = new Handler();
+            _garageMenu =
+            [
+                ExitProgram, 
+                ParkVehicle, 
+                RemoveVehicle, 
+                PopulateGarage
+            ];
+            CreateGarage();
+            ShowGarageMenu();
+        }
+
+        private void GarageInfo()
+        {
+            //TODO fix "free capacity"
+            UI.Clear();
+            UI.WriteLine($"Garage info: total capacity = {_garage.Capacity}, free capacity: {_garage.Capacity}");
+        }
+
+        private void ShowGarageMenu()
+        {
+            GarageInfo();
+            UI.GarageMenu();
+            int val = UI.ReadInt($"Error, Valid numers are 0-{_garageMenu.Count-1}", _garageMenu.Count-1);
+            _garageMenu[val].Invoke();
+        }
+
+        private void CreateGarage()
+        {
+            UI.CreateGarageMenu();
+            int capacity = UI.ReadInt("Error, Valid numbers are 1-1000", 1000, 1);
+            _garage = new Garage<Vehicle>(capacity);
+        }
+
+        private void ParkVehicle()
+        {
+            UI.WriteLine("Park");
+        }
+
+        private void RemoveVehicle()
+        {
+            UI.WriteLine("Remove");
+        }
+
+        private void PopulateGarage()
+        {
+            UI.WriteLine("Populate");
+        }
+
+        private void ExitProgram()
+        {
+            UI.WriteLine("Exit");
         }
     }
 }
