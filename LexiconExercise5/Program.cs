229c2256c0b06bc43a757271687a6f26b0cf9c8f
Jimmy <212524470+jimmy-codix@users.noreply.github.com>
2025-06-16 07:49:15 +0200

    Code moved to Manager.

--- LexiconExercise5/Program.cs
+++ LexiconExercise5/Program.cs
@@ -7,19 +7,29 @@ namespace LexiconExercise5
     {
         static void Main(string[] args)
         {
-            var mgr = new Manager();
-            var ui = new UI();
-            var handler = new Handler();
-            IGarage<Vehicle> grg = new Garage<Vehicle>(10);
-            int a = grg.Count;
-            Console.WriteLine(a.ToString());
-            Vehicle bt = new Boat();
-            grg.Park(bt);
-            foreach (var item in grg)
-            {
-                Console.WriteLine($"Nummer {item.UnitSize}");
-            }
-            Console.WriteLine(grg.Count.ToString());
+            new Manager();
+            //Vehicle[] ts = new Vechicle[]
+            //Console.WriteLine(MENU_GARAGE);
+            ////var i = Console.ReadKey();
+            //var i = Console.ReadKey(intercept: true).Key;
+            //Console.WriteLine($" you wrote: {i}");
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.ReadLine();
+            
+            //var mgr = new Manager();
+            //var ui = new UI();
+            //var handler = new Handler();
+            //IGarage<Vehicle> grg = new Garage<Vehicle>(10);
+            //int a = grg.Count;
+            //Console.WriteLine(a.ToString());
+            //Vehicle bt = new Boat();
+            //grg.Park(bt);
+            //foreach (var item in grg)
+            //{
+            //    Console.WriteLine($"Nummer {item.UnitSize}");
+            //}
+            //Console.WriteLine(grg.Count.ToString());
         }
     }
 }
