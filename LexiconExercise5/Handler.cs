befb2bbdaba8ecc929becb210a36ce820c131119
Jimmy <212524470+jimmy-codix@users.noreply.github.com>
2025-06-16 18:50:40 +0200

    Added methods for interacting with Garage.

--- LexiconExercise5/Handler.cs
+++ LexiconExercise5/Handler.cs
@@ -1,9 +1,44 @@
-﻿namespace LexiconExercise5
+﻿using LexiconExercise5.Interfaces;
+using LexiconExercise5.Vehicles;
+
+namespace LexiconExercise5
 {
     internal class Handler
     {
-        public Handler()
+        //Handler sksa sköta mera
+        private Garage<Vehicle> _garage;
+        public Handler() { }
+
+        //TODO Denna kan flyttas till konstruktorn.
+        public void CreateGarage(int capacity)
         {
+            _garage = new Garage<Vehicle>(capacity);
+        }
+
+        public bool ParkVehicle(Vehicle vehicle)
+        {
+            return _garage.Park(vehicle);
+        }
+
+        internal int GetGarageCapacity()
+        {
+            return _garage.Capacity;
+        }
+
+        internal int GetGarageFreeCapacity()
+        {
+            return _garage.FreeCapacity;
+        }
+
+        public List<Vehicle> GetVehicles()
+        {
+            List<Vehicle> list = new List<Vehicle>();
+            foreach (var item in _garage)
+            {
+                list.Add(item);
+            }
+
+            return list;
         }
     }
 }
