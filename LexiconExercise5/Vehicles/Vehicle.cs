359fa4f1a16131e4a98ba5d4b4ae973b76e68905
Jimmy <212524470+jimmy-codix@users.noreply.github.com>
2025-06-16 18:46:11 +0200

    Added Enum for vehicletype (used in menus) and an override of ToString.

--- LexiconExercise5/Vehicles/Vehicle.cs
+++ LexiconExercise5/Vehicles/Vehicle.cs
@@ -1,13 +1,33 @@
 ﻿namespace LexiconExercise5.Vehicles
 {
+    //TODO move this out
+    enum VehicleType
+    {
+        Airplane,
+        Boat,
+        Bus,
+        Car,
+        Motorcycle
+    }
     internal abstract class Vehicle
     {
         //TODO may be readonly?
-        public int UnitSize { get; protected set; } = 1;
+        //TODO unitsize is always 1. At this moment it will be 1 until i extend it.
+        public int UnitSize { get; private set; } = 1;
+        public string RegistrationNr { get; private set; }
 
-        public Vehicle(int unitSize) 
+        public Vehicle(string registrationNr, int unitSize) 
         { 
             UnitSize = unitSize;
+            if (registrationNr.Length < 1 || registrationNr.Length > 6)
+                throw new ArgumentException("Registration number needs to be between 1 and 6 characters long.");
+
+            RegistrationNr = registrationNr;
+        }
+
+        public override string ToString()
+        { 
+            return this.GetType().Name + " " + this.RegistrationNr;
         }
 
     }
