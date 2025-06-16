9ab1d42f61613c7e66fc6ced26cc24dc35ed8658
Jimmy <212524470+jimmy-codix@users.noreply.github.com>
2025-06-16 07:48:47 +0200

    Added some UI methods.

--- LexiconExercise5/UI.cs
+++ LexiconExercise5/UI.cs
@@ -1,9 +1,67 @@
-﻿namespace LexiconExercise5
+﻿
+
+namespace LexiconExercise5
 {
-    internal class UI
+    public static class UI
     {
-        public UI()
+        const string GARAGE_CREATE_INFO = "Please enter the capacaty of your new garage:";
+        const string GARAGE_MENU = @"
+        1. Park a vehicle
+        2. Remove a vehicle
+        3. Populate garage with vehicles
+        0. Exit program";
+
+        public static int ReadInt(string errorText, int max = 100, int min = 0)
         {
+            do
+            {
+                int value;
+                string? raw = Console.ReadLine();
+                if (raw == null)
+                    continue;
+                if (int.TryParse(raw, out value))
+                {
+                    if (value >= min && value <= max)
+                        return value;
+                    else
+                        Console.WriteLine(errorText);
+                }
+                else
+                {
+                    Console.WriteLine(errorText);
+                }
+            } while (true);
+
+        }
+
+        public static void CreateGarageMenu()
+        {
+            Console.WriteLine(GARAGE_CREATE_INFO);
+        }
+
+        public static void Clear()
+        {
+            Console.Clear();
+        }
+
+        public static void Write(string text)
+        {
+            Console.Write(text);
+        }
+
+        public static void WriteLine(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public static string? ReadLine()
+        {
+            return Console.ReadLine();
+        }
+
+        internal static void GarageMenu()
+        {
+            WriteLine(GARAGE_MENU);
         }
     }
 }
