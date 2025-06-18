bea47e0c43babb846f4c58b5709de9d865f18940
Jimmy <212524470+jimmy-codix@users.noreply.github.com>
2025-06-18 13:56:41 +0200

    Better Search logic.

--- LexiconExercise5/ManagerSearch.cs
+++ LexiconExercise5/ManagerSearch.cs
@@ -10,44 +10,53 @@ namespace LexiconExercise5
 {
     internal partial class Manager
     {
-        private void SearchMotorcycle()
+        private void SearchVehicle(Func<IEnumerable<Vehicle>, Vehicle[]> exp)
         {
-            GarageInfo();
-            int wheels = UI.ReadInt("How many wheels does the motocycle have?");
-            //Vehicle[] arr = _handler.SearchMotorCycle(wheels);
-            Func<IEnumerable<Vehicle>, Vehicle[]> exp = y => y.OfType<Motorcycle>().Where(x => x.NrOfWheels == wheels).ToArray();
             Vehicle[] arr = _handler.TestSearch(exp);
             if (arr.Length == 0)
             {
-                UI.WriteLine($"No motorcycle could be found with {wheels} wheels.");
+                UI.WriteLine($"No match was found.");
                 return;
             }
 
-            UI.WriteLine("The following motorcycles was found:");
+            UI.WriteLine("The following matches was found:");
             foreach (var vehicle in arr)
-            {
                 UI.WriteLine(vehicle.Details());
-            }
+        }
+
+        private void SearchMotorcycle()
+        {
+            GarageInfo();
+            int wheels = UI.ReadInt("How many wheels does the motocycle have?",Motorcycle.WHEELS_MAX,Motorcycle.WHEELS_MIN);
+            SearchVehicle(y => y.OfType<Motorcycle>().Where(x => x.NrOfWheels == wheels).ToArray());
         }
 
         private void SearchCar()
         {
-            throw new NotImplementedException();
+            GarageInfo();
+            int nr = UI.ReadInt("How many doors does the car have?",Car.DOORS_MAX,Car.DOORS_MIN);
+            SearchVehicle(y => y.OfType<Car>().Where(x => x.NrCarDoors == nr).ToArray());
         }
 
         private void SearchBus()
         {
-            throw new NotImplementedException();
+            GarageInfo();
+            int nr = UI.ReadInt("Does the bus have a toilet? Enter 0 for No and 1 for Yes.", 1, 0);
+            SearchVehicle(y => y.OfType<Bus>().Where(x => x.HasToilet == Convert.ToBoolean(nr)).ToArray());
         }
 
         private void SearchBoat()
         {
-            throw new NotImplementedException();
+            GarageInfo();
+            int nr = UI.ReadInt("Is the boat sinkable Enter 0 for No and 1 for Yes?", 1, 0);
+            SearchVehicle(y => y.OfType<Boat>().Where(x => x.IsSinkable == Convert.ToBoolean(nr)).ToArray());
         }
 
         private void SearchAirplane()
         {
-            throw new NotImplementedException();
+            GarageInfo();
+            int nr = UI.ReadInt("How many seats does the airplane have?", Airplane.SEATS_MAX, Airplane.SEATS_MIN);
+            SearchVehicle(y => y.OfType<Airplane>().Where(x => x.NrOfSeats == nr).ToArray());
         }
     }
 }
