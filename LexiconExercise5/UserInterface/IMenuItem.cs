09e41882f18b5f65a14ce2205fba7840090e81f8
Jimmy <212524470+jimmy-codix@users.noreply.github.com>
2025-06-23 09:11:15 +0200

    Added Interface for future use.

---
+++ LexiconExercise5/UserInterface/IMenuItem.cs
@@ -0,0 +1,10 @@
+﻿
+using LexiconExercise5.Interfaces;
+
+namespace LexiconExercise5.UserInterface
+{
+    internal interface IMenuItem : IMenuItemBase
+    {
+        Action Caller { get; }
+    }
+}
